68da1e34993b4bba7f4dfb6f9fe7e6b9540d8055
unknown <sunitkumarpattnaik22@gmail.com>
2024-04-28 23:09:55 +0530

    Complete Ecommerce Php Project With Documentation

---
+++ Ecommerce/customer/pay_offline.php
@@ -0,0 +1,43 @@
+<center><!--  center Begin  -->
+    
+    <h1> Pay Offline Using Method </h1>
+    
+    <p class="text-muted">
+        
+        If you have any questions, feel free to <a href="../contact.php">Contact Us</a>. Our Customer Service work <strong>24/7</strong>
+        
+    </p>
+    
+</center><!--  center Finish  -->
+
+
+<hr>
+
+
+<div class="table-responsive"><!--  table-responsive Begin  -->
+    
+    <table class="table table-bordered table-hover table-striped"><!--  table table-bordered table-hover Begin  -->
+        
+        <thead><!--  thead Begin  -->
+            
+            <tr><!--  tr Begin  -->
+            
+                <th> Bank Account Details: </th>
+                <th> Easy Paisa,Mobi Cash Details: </th>
+                <th> Bank  Details: </th>
+
+            </tr><!--  tr Finish  -->
+            
+        </thead><!--  thead Finish  -->
+        
+        <tbody><!--  tbody Begin  -->
+           
+           <td> Bank Name: SBI | Account No: 917539025287 | Branch Name:BHUBANESWAR | Branch Code: 1498 </td>
+           <td> PAYTM US  | Mobile No: 91-7539025287 | Name: Sunit </td>
+           <td> Real Name: SUNIT PATTANIK | Mobile No: 91-7539025287 Country: INDIA | Name: SUNIT | PAYTM US</td>
+            
+        </tbody><!--  tbody Finish  -->
+        
+    </table><!--  table table-bordered table-hover Finish  -->
+    
+</div><!--  table-responsive Finish  -->
